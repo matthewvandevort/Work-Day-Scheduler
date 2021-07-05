29235f3647443ac20f6930d0d903702c1b019184
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-07-05 11:59:52 -0400

    updated hour block colors

--- assets/script.js
+++ assets/script.js
@@ -9,13 +9,50 @@ var hr4 = $('#16');
 var hr5 = $('#17');
 
 
-
-
 function updateTime() {
     var now = moment();
-    $('#currentDay').text(now.format('dddd, MMMM Do, YYYY hh:mm:ss a'));
-
-    
+    $('#currentDay').text(now.format('dddd MMMM Do, YYYY hh:mm:ss a'));
+   
 }
 setInterval(updateTime, 1000);
 updateTime();
+
+
+$(document).ready(function () {
+
+    $('.saveBtn').on('click', function () {
+
+        var toDo = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+
+        localStorage.setItem(time, toDo);
+    })
+
+    function getTime() {
+
+        var currentTime = moment().hour();
+
+
+        $('time-block').each(function () {
+            var hourBox = parseInt($(this).attr('id').split('hour')[1]);
+
+            if (hourBox < currentTime) {
+                $(this).removeClass('future');
+                $(this).removeClass('present');
+                $(this).addClass('past');
+            }
+            else if (hourBox === currentTime) {
+                $(this).removeClass('future');
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            }
+            else {
+                $(this).removeClass('present');
+                $(this).removeClass('past');
+                $(this).addClass('future');
+                
+            }     
+        })    
+    }   
+    getTime();
+})    
