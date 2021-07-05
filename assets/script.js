69940e4b050eddb339675289531f5fe102b5fdda
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-07-05 13:49:29 -0400

    added comments to js, css, html

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,36 @@
-
+// Integrates the time on the page using moment.js.
 function updateTime() {
     var now = moment();
     $('#currentDay').text(now.format('dddd MMMM Do, YYYY hh:mm:ss a'));
    
 }
+// Updates the time every second.
 setInterval(updateTime, 1000);
 updateTime();
 
-
+// Button event listener.
 $(document).ready(function () {
 
     $('.saveBtn').on('click', function () {
 
+        // This gets the values of the description box.
         var toDo = $(this).siblings('.description').val();
         var time = $(this).parent().attr('id');
 
+        // This will save the text in the description box in local storage
         localStorage.setItem(time, toDo);
     })
 
+    // Function to get hours so we can change the colors of the description box.
     function getTime() {
-
+        // This variable stores the current hours using moment.
         var currentTime = moment().hour();
 
-
+        // Now we loop over each time-block, taking the hour, and storing it in a variable of hourBox so it can be compared to the variable currentTime.
         $('.time-block').each(function () {
             var hourBox = parseInt($(this).attr('id').split('hour')[1]);
 
+            // This if/else statement will check the time and add or remove classes for the backgroung colors.
             if (hourBox < currentTime) {
                 $(this).removeClass('present');
                 $(this).removeClass('future');
@@ -45,7 +50,7 @@ $(document).ready(function () {
         })    
     } 
     
-
+    // This will retrieve items from local storage if they exist.
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -56,5 +61,6 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
+    // Calling the function getTime.
     getTime();
 })    
